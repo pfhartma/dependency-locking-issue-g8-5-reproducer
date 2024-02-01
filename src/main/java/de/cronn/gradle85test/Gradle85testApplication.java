d2b87f0f637ecb5089c7b34b6c932050eba5010d
Paul-Fiete Hartmann <paul-fiete.hartmann@cronn.de>
2024-02-01 09:14:23 +0100

    init

---
+++ src/main/java/de/cronn/gradle85test/Gradle85testApplication.java
@@ -0,0 +1,13 @@
+package de.cronn.gradle85test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gradle85testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gradle85testApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/cronn/gradle85test/Gradle85testApplicationTests.java
@@ -0,0 +1,13 @@
+package de.cronn.gradle85test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gradle85testApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
